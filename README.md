170a7739361214a57c0f9c442b1b5967333f62be
Eugenia Oshurko <yarutoua@gmail.com>
2018-06-21 13:52:26 +0200

    Modified: README and setup.py

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 # KAMIStudio
 
-KAMIStudio is a bio-curation platform for modelling cellular signalling based on [KAMI](https://github.com/Kappa-Dev/KAMI) library.
+KAMIStudio is a web-based bio-curation environment for modelling cellular signalling based on [KAMI](https://github.com/Kappa-Dev/KAMI) library.
 
+
+## Installation
+
+Clone the repo:
+
+```
+git clone git@github.com:Kappa-Dev/KAMIStudio.git
+cd KAMIStudio
+```
+
+
+Install KAMIStudio with setuptools:
+
+```
+python setup.py install
+```
+
+
+In order to run KAMIStudion export an environment variable that tells Flask where to find the application instance:
+
+```
+export FLASK_APP=kamistudio
+````
+
+Finally run the application:
+
+```
+flask run
+```
--- setup.py
+++ setup.py
@@ -8,9 +8,23 @@ setup(name='KAMIStudio',
       description='Bio-curation tool for cellular signalling',
       author='Russ Harmer, Yves-Stan Le Cornec, Sebastien Legare, Eugenia Oshurko',
       license='MIT License',
-      packages=['kamistudio'],
+      packages=[
+          'kamistudio',
+          'kamistudio.action_graph',
+          'kamistudio.home',
+          'kamistudio.model',
+          'kamistudio.nuggets'],
       package_dir={},
-      package_data={},
+      package_data={
+          'kamistudio': [
+              'templates/*', 'static/*',
+              'static/bootstrap/*', 'static/css/*',
+              'static/js/*'],
+          'kamistudio.action_graph': ['templates/*', 'static/*'],
+          'kamistudio.home': ['templates/*', 'static/*'],
+          'kamistudio.model': ['templates/*', 'static/*'],
+          'kamistudio.nuggets': ['templates/*', 'static/*'],
+      },
       include_package_data=True,
       zip_safe=False,
       install_requires=[
@@ -21,4 +35,8 @@ setup(name='KAMIStudio',
           'lxml',
           'jpype1',
           'flask_cors'
+      ],
+      dependency_links=[
+          'git+https://github.com/Kappa-Dev/ReGraph@master#egg=regraph-1.0',
+          'git+https://github.com/Kappa-Dev/KAMI@master#egg=kami-1.2.0'
       ])
