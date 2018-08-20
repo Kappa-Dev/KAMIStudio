4d3b1bc8ea10654b5793447b5ddc6d8ec1c5c256
Eugenia Oshurko <yarutoua@gmail.com>
2018-08-20 18:15:03 +0200

    Modified: 'add_nugget' view of 'model' subapp, addition of nuggets to the model is finalised

--- kamistudio/model/views.py
+++ kamistudio/model/views.py
@@ -1,10 +1,11 @@
 """Views of home blueprint."""
 import json
 
-from flask import render_template, Blueprint, request, redirect, url_for
+from flask import (render_template, Blueprint, request, session, redirect,
+                   url_for)
 from flask import current_app as app
 
-from regraph import (graph_to_d3_json, print_graph)
+from regraph import graph_to_d3_json
 
 from kami.export.old_kami import ag_to_edge_list
 from kami.aggregation.generators import generate_from_interaction
@@ -38,12 +39,15 @@ def model_view(hierarchy_id):
     nugget_desc = {}
     for nugget in app.hierarchies[hierarchy_id].nuggets():
         if 'desc' in app.hierarchies[hierarchy_id].node[nugget].attrs.keys():
-            nugget_desc[nugget] = list(
-                app.hierarchies[hierarchy_id].node[nugget].attrs['desc'])[0]
+            if type(app.hierarchies[hierarchy_id].node[nugget].attrs['desc']) == str:
+                nugget_desc[nugget] = app.hierarchies[hierarchy_id].node[nugget].attrs['desc']
+            else:
+                nugget_desc[nugget] = list(
+                    app.hierarchies[hierarchy_id].node[nugget].attrs['desc'])[0]
         else:
             nugget_desc[nugget] = ""
 
-    return render_template("hierarchy.html",
+    return render_template("model.html",
                            hierarchy_id=hierarchy_id,
                            hierarchies=app.hierarchies,
                            action_graph_edgelist=new_edgelist,
@@ -64,14 +68,17 @@ def add_interaction(hierarchy_id):
         nugget, nugget_type = generate_from_interaction(
             app.hierarchies[hierarchy_id], interaction)
 
+        session["nugget"] = nugget
+        session["nugget_type"] = nugget_type
+        session.modified = True
+
         template_relation = {}
         for k, v in nugget.template_rel.items():
             for vv in v:
                 template_relation[vv] = k
-        print(template_relation)
 
         return render_template(
-            "nugget_editor.html",
+            "nugget_preview.html",
             new_nugget=True,
             hierarchy_id=hierarchy_id,
             hierarchies=app.hierarchies,
@@ -80,3 +87,17 @@ def add_interaction(hierarchy_id):
             nugget_meta_typing=json.dumps(nugget.meta_typing),
             nugget_ag_typing=json.dumps(nugget.ag_typing),
             nugget_template_rel=json.dumps(template_relation))
+
+
+@model_blueprint.route("/model/<hierarchy_id>/add_nugget",
+                       methods=["GET"])
+def add_nugget(hierarchy_id, add_agents=True,
+               anatomize=True, apply_semantics=True):
+    nugget_id = app.hierarchies[hierarchy_id].add_nugget(
+        session["nugget"], session["nugget_type"],
+        add_agents=add_agents,
+        anatomize=anatomize,
+        apply_semantics=apply_semantics)
+    session.pop('nugget', None)
+    session.pop('nugget_type', None)
+    return redirect(url_for('model.model_view', hierarchy_id=hierarchy_id))
