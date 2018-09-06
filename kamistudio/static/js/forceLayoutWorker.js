e185ed54fe885d15d731769127fe04f348c97853
Eugenia Oshurko <yarutoua@gmail.com>
2018-09-06 18:36:23 +0200

    AG visualisation web worker

---
+++ kamistudio/static/js/forceLayoutWorker.js
@@ -0,0 +1,32 @@
+importScripts("https://d3js.org/d3.v4.min.js");
+
+
+onmessage = function(event) {
+  var nodes = event.data.nodes,
+      links = event.data.links,
+      width = event.data.width,
+      height = event.data.height;
+
+  var simulation = d3.forceSimulation()
+      .force("charge", d3.forceManyBody().strength(-40))
+      .force("link",
+             d3.forceLink()
+                  .id(function(d) { return d.id; })
+                  .strength(function(d) {return d.strength; })
+                  .distance(function(d) {return d.distance; }))
+      .force("center", d3.forceCenter(width / 2, height / 2))
+      .force("collide",d3.forceCollide().strength(1.8).radius(
+          function(d) {return d.radius})
+        );
+
+  simulation.nodes(nodes);
+  simulation.force("link").links(links);
+  simulation.stop();
+
+  for (var i = 0, n = Math.ceil(Math.log(simulation.alphaMin()) / Math.log(1 - simulation.alphaDecay())); i < n; ++i) {
+    postMessage({type: "tick", progress: i / n});
+    simulation.tick();
+  }
+
+  postMessage({type: "end", nodes: nodes, links: links});
+};
