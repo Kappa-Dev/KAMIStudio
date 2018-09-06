6139b9ffc5b827367a4d59a84fe5691b52376005
Eugenia Oshurko <yarutoua@gmail.com>
2018-09-06 18:27:06 +0200

    Fixed bug in fetching action graph

--- kamistudio/action_graph/views.py
+++ kamistudio/action_graph/views.py
@@ -10,11 +10,24 @@ action_graph_blueprint = Blueprint(
 
 
 @action_graph_blueprint.route("/model/<hierarchy_id>/raw-action-graph")
-def raw_action_graph_json(hierarchy_id):
+def raw_action_graph_json(hierarchy_id, attrs=False):
+    """Handle the raw json action graph representation."""
     hierarchy = app.hierarchies[hierarchy_id]
 
-    data = {}
-    data["actionGraph"] = graph_to_d3_json(hierarchy.action_graph)
-    data["metaTyping"] = hierarchy.action_graph_typing
+    if "node_positioning" in hierarchy.attrs.keys():
+        node_positioning = list(hierarchy.attrs["node_positioning"].fset)
 
+    else:
+        node_positioning = {}
+
+    data = {}
+
+    if (hierarchy.action_graph):
+        data["actionGraph"] = graph_to_d3_json(
+            hierarchy.action_graph, attrs, ["hgnc_symbol"])
+    else:
+        data["actionGraph"] = {"links": [], "nodes": []}
+
+    data["metaTyping"] = hierarchy.action_graph_typing
+    data["nodePosition"] = node_positioning
     return jsonify(data), 200
