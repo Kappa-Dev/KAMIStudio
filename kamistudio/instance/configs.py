763666946efbffc181fa18ef383ea16b2dc8353f
Eugenia Oshurko <yarutoua@gmail.com>
2019-03-12 18:00:47 +0100

    Fixed setup.py + config files has mongodb settings

--- kamistudio/instance/configs.py
+++ kamistudio/instance/configs.py
@@ -4,3 +4,7 @@ DEBUG = True
 UPLOAD_FOLDER = './kamistudio/uploads'
 DOWNLOAD_FOLDER = "kamistudio/downloads"
 ALLOWED_EXTENSIONS = set(['json'])
+NEO4J_URI = "bolt://127.0.0.1:7687"
+NEO4J_USER = "neo4j"
+NEO4J_PWD = "admin"
+MONGO_URI = "mongodb://127.0.0.1:27017"
