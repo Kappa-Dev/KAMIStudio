86d3ae8b65e1bdca07bae413bd3e5ec3beb97c7a
Eugenia Oshurko <yarutoua@gmail.com>
2019-04-12 19:07:15 +0200

    Added config py

--- kamistudio/instance/config.py
+++ kamistudio/instance/config.py
@@ -1,7 +1,7 @@
 """Default KAMIStudio instance config."""
 NEO4J_URI = "bolt://127.0.0.1:7687"
 NEO4J_USER = "neo4j"
-NEO4J_PWD = "neo4j"
+NEO4J_PWD = "admin"
 MONGO_URI = "mongodb://127.0.0.1:27017"
 
 # Generate a new secret key
