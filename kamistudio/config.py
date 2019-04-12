193d9df47907e5fa7f71eff1f8cbb1f66bc784e0
Eugenia Oshurko <yarutoua@gmail.com>
2019-04-12 19:06:59 +0200

    Modified config py

--- kamistudio/config.py
+++ kamistudio/config.py
@@ -2,10 +2,12 @@
 
 ALLOWED_EXTENSIONS = set(['json'])
 
-DEBUG = False
+DEBUG = True
 
 DOWNLOAD_FOLDER = 'kamistudio/downloads'
 
-READ_ONLY = True
+READ_ONLY = False
 
 SESSION_TYPE = 'filesystem'
+
+UPLOAD_FOLDER = 'kamistudio/uploads'
