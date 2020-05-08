2a3b839e57ebc8a4fc508387a47bfda4153cf116
eugeniashurko <yarutoua@gmail.com>
2020-05-08 14:28:45 +0200

    Fixes

--- kamistudio/static/js/utils.jsx
+++ kamistudio/static/js/utils.jsx
@@ -198,3 +198,9 @@ function generateEdgeMetaDataItems(sourceId, targetId,
 	}
 	return [message, items, data];
 }
+
+$('tr').click( function() {
+    window.location = $(this).find('a').attr('href');
+}).hover( function() {
+    $(this).toggleClass('hover');
+});
